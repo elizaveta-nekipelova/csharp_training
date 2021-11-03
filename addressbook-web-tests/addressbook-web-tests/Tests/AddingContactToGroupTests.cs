f7b142db657604e3654cab9fb773388747f20f80
Elizaveta Brezgunova <elizaveta.brezgunova@euromoneyplc.com>
2021-11-03 23:08:57 +0300

    Exercise 17: Homework review fix

--- addressbook-web-tests/addressbook-web-tests/Tests/AddingContactToGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/AddingContactToGroupTests.cs
@@ -12,13 +12,95 @@ namespace addressbook_web_tests
         [Test]
         public void TestAddingContactToGroup()
         {
-            GroupData group = GroupData.GetAll()[0];
-            List<ContactData> oldList = group.GetContacts();
+            if (GroupData.GetAll().Count() == 0)
+            {
+                GroupData newGroup = new GroupData("TestAddingContactToGroupTest");
+                newGroup.Header = "header";
+                newGroup.Footer = "footer";
+
+                app.Groups.Create(newGroup);
+            }
+
+            if (ContactData.GetAll().Count() == 0)
+            {
+                ContactData newContact = new ContactData("Test", "User");
+                app.Contacts.Create(newContact);
+            }
+
+            List<GroupData> groups = GroupData.GetAll();
+            List<ContactData> oldList = new List<ContactData>();
+
+            // this is the check for the case when all contacts are assigned to every group
+            // I go throught all groups and check if there are some missing contact
+            // if no then new contact created
+            int index = -1;
+            for (int i = 0; i < groups.Count(); i++)
+            {
+                oldList = groups[i].GetContacts();
+
+                if (ContactData.GetAll().Except(oldList).Count() != 0)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1)
+            {
+                ContactData newContact = new ContactData("User", GenerateRandomString(5));
+                app.Contacts.Create(newContact);
+                index = 0;
+            }
+            // end of checking
+
+            GroupData groupToAddContact = groups[index];
             ContactData contact = ContactData.GetAll().Except(oldList).First();
 
-            app.Contacts.AddContactToGroup(contact, group);
+            app.Contacts.AddContactToGroup(contact, groupToAddContact);
 
-            List<ContactData> newList = group.GetContacts();
+            List<ContactData> newList = groupToAddContact.GetContacts();
+            oldList.Add(contact);
+            newList.Sort();
+            oldList.Sort();
+
+            Assert.AreEqual(oldList, newList);
+        }
+
+
+        [Test]
+        public void TestAddingContactToGroup2()
+        {
+            if (GroupData.GetAll().Count() == 0)
+            {
+                GroupData newGroup = new GroupData("TestAddingContactToGroupTest");
+                newGroup.Header = "header";
+                newGroup.Footer = "footer";
+
+                app.Groups.Create(newGroup);
+            }
+
+            if (ContactData.GetAll().Count() == 0)
+            {
+                ContactData newContact = new ContactData("Test", "User");
+                app.Contacts.Create(newContact);
+            }
+
+            var groups = GroupData.GetAll().Where(g => ContactData.GetAll().Except(g.GetContacts()).Count() != 0).ToList();
+
+            if (groups.Count() == 0)
+            {
+                ContactData newContact = new ContactData("User", GenerateRandomString(5));
+                app.Contacts.Create(newContact);
+                groups.Add(GroupData.GetAll().First());
+            }
+
+            GroupData groupToAddContact = groups.First();
+            List<ContactData> oldList = groupToAddContact.GetContacts();
+            ContactData contact = ContactData.GetAll().Except(oldList).First();
+
+            app.Contacts.AddContactToGroup(contact, groupToAddContact);
+
+            List<ContactData> newList = groupToAddContact.GetContacts();
             oldList.Add(contact);
             newList.Sort();
             oldList.Sort();
